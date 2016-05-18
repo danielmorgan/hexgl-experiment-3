f6f6e106cff81c3bf3f5b7b0dfa0d8f880889198
Daniel Morgan <me@danielmorgan.co.uk>
2016-05-18 18:43:52 +0100

    Add controls for Stats.js display

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,12 +15,23 @@ class Game {
         this.stage = new PIXI.Container();
         this.$container = $('#game');
         this.stats = new Stats();
+        this.stats.mode = 0;
 
         this.$container.append(this.renderer.view);
         $('body').append($(this.stats.dom).addClass('stats'));
 
+        this.bindEvents();
         this.setTheStage();
         this.update();
+    }
+
+    bindEvents() {
+        // Stats controls
+        console.info("Stats.js\n===================\n[H] Show/hide panel\n[J] Change panel")
+        $(window).on('keydown', e => {
+            if (e.keyCode === 72) $(this.stats.dom).toggle();
+            if (e.keyCode === 74) this.stats.showPanel(this.stats.mode++ % 3);
+        })
     }
 
     setTheStage() {
